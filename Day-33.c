c051460c666bb9e1dce89440905c2cf5ee42dab5
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-22 23:52:04 +0600

    Update Day-33.c

--- Day-33.c
+++ Day-33.c
@@ -36,11 +36,32 @@ i++;
 }
 
 
+
+
+
+
+// For loop multiplication table
+
+int n;
+printf("Enter the number : ");
+scanf("%d",&n);
+
+for(int i = 1;i<=10;i++){
+
+printf("%d * %d = %d\n",n,i,n*i);
+
+
+}
+
+
+
+
   
 
     
     return 0;
 }
+
 
 
 
