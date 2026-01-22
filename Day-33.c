ba83dac92a3559cbc2fb1c936a821c79b609b46d
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-22 23:52:53 +0600

    Update Day-33.c

--- Day-33.c
+++ Day-33.c
@@ -43,11 +43,31 @@ printf("%d * %d = %d\n",n,i,n*i);
 
 
 
+
+
+
+
+  
+  // while loop multiplication table
+
+int n;
+printf("Enter the number : ");
+scanf("%d",&n);
+
+int i = 1;
+while(i<=10){
+
+    printf("%d * %d = %d\n",n,i,n*i);
+i++;
+}
+
+
   
 
     
     return 0;
 }
+
 
 
 
