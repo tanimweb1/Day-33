ac041f71737c74b24174dc4a0cfeeef5d4dc8f8b
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-22 23:51:52 +0600

    Refactor Day-33.c by removing for loop multiplication
    Removed the multiplication table using for loop and simplified the code structure.

--- Day-33.c
+++ Day-33.c
@@ -18,19 +18,6 @@ break;
 
 
 
-// For loop multiplication table
-
-int n;
-printf("Enter the number : ");
-scanf("%d",&n);
-
-for(int i = 1;i<=10;i++){
-
-printf("%d * %d = %d\n",n,i,n*i);
-
-
-}
-
 
 
 
@@ -54,5 +41,6 @@ i++;
     
     return 0;
 }
+
 
 
